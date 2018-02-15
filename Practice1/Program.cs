0bbbdb8ef16eb5787f7f697ac11bd01b8765ddb3
Brenden Seidel <brenseid@gmail.com>
2018-02-15 11:14:42 -0500

    Initial commit

--- Practice1/Program.cs
+++ Practice1/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 
-namespace Practice1
+namespace Treehouse.PracticeSession
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            // TODO Declare and assign a "thing" string variable.
+            // Example: "movie", "book", "color", etc.
+            string thing = "book";
+
+            // TODO Prompt the user with the text "What is your name?" 
+            // and assign their value to a "name" string variable.
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            // TODO Prompt the user with the text 
+            // "What is your favorite <thing>?" 
+            // and assign their value to a "favoriteThing" string variable.
+            Console.WriteLine(value: "What is your favorite " + thing + " ?");
+            string favoriteThing = Console.ReadLine();
+
+            // TODO Write the user's name and favorite thing to the console.
+            // Example: "My name is James and my favorite movie is Toy Story."
+            Console.WriteLine("My name is " + name + " and my favorite " + thing + " is " + favoriteThing + ".");
+            Console.WriteLine("Press any key to close");
+            Console.ReadKey();
         }
     }
 }
